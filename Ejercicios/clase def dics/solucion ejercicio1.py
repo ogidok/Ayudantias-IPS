bf5ae3c2a3365c1e04e5ff6f787f4bac93472787
ogidokogidoksan <164224379+ogidok@users.noreply.github.com>
2025-06-02 18:38:28 -0400

    Add files via upload

---
+++ Ejercicios/clase def dics/solucion ejercicio1.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+def sumar_valores(diccionarios):
+    total=0
+
+    for valor in diccionarios.values():
+        total+= valor
+    return total
+
+
+print(sumar_valores({"a":2,"b":1,"c":3}))
+
+
+
+
+
+
+
+
+
