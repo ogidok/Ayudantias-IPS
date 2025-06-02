b7091bfe34fc948efecb28bfc7619ade1dd0333d
ogidokogidoksan <164224379+ogidok@users.noreply.github.com>
2025-06-02 18:40:36 -0400

    Add files via upload

---
+++ Ejercicios/clase listas fun/claje.py
@@ -0,0 +1,58 @@
+# def sumanums(lista):
+#     suma=0
+#     for item in lista:
+#         suma+=item
+    
+#     return suma
+
+
+
+# print((sumanums([1,2,4,5,5])))
+
+
+
+# def buscar_elemento(lista, valor):
+    
+#     for elemento in lista:
+#         if elemento == valor:
+#             return True    
+#     return False  
+            
+# print(buscar_elemento([1,23,4,5],1))
+
+
+
+# def contar_vocales(lista):
+#     cantidadvocales=0
+#     vocales="aeiou"
+#     for item in lista:
+#         item.lower()
+#         for letra in item:
+#             if letra in vocales:
+#                 cantidadvocales+=1
+#             else:
+#                 pass
+
+#     return cantidadvocales
+
+
+
+# print(contar_vocales(["eljavi es vio","holaaa"]))
+
+# def filtrar_mayores(lista, umbral):
+#     resultado = []
+#     for numero in lista:
+#         if numero > umbral:
+#             resultado.append(numero)
+#     return resultado
+# # # # Ejemplo de uso
+# print(filtrar_mayores([10, 3, 25, 8, 30,80], 10))
+# # Salida esperada: [25, 30]
+
+
+
+
+
+
+
+
